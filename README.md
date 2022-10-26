be7807641aaaa0a4c324e76b9b1b8e3704a90763
Thevindu <kulasinghet@gmail.com>
2022-10-26 14:02:23 +0530

    Initialized the project structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MedEx
+
+## Added dependencies
+1. Composer
+1.1 Autoloader
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<! Doctype html>
+<html lang="en">
+<head>
+    <title>MedEx</title>
+</head>
+<body>
+    <h1>MedEx</h1>
+    <p>MedEx is a web application that allows you to manage your medical records.</p>
+    <p>It is currently under development.</p>
+</body>
+</html>
