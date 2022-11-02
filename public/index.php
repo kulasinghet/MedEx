7113868f68f8eb0d71c278223320f9443b2770d5
Thevindu <kulasinghet@gmail.com>
2022-11-02 07:17:34 +0530

    Initialized project

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <h1>MedEx</h1>
-    <p>MedEx is a web application that allows you to manage your medical records.</p>
-    <p>It is currently under development.</p>
+    <p>MedEx is a web application that allows you to streamline your medical supply chain.</p>
+    <p>This page is currently under development.</p>
 </body>
 </html>
