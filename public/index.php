a55bf8b613585b205aefa41f81d57894abcf8dae
Thevindu <kulasinghet@gmail.com>
2022-11-14 16:38:51 +0530

    Added carousal to the homepage

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <html lang="en">
 <head>
     <title>MedEx</title>
-    <link href="css/footer.css"  type="text/css" rel="stylesheet"/>
-    <link href="css/carousel.css"  type="text/css" rel="stylesheet"/>
+    <link href="css/homepage/footer.css" type="text/css" rel="stylesheet"/>
+    <link href="css/homepage/carousel.css" type="text/css" rel="stylesheet"/>
+    <link href="css/homepage/navbar.css" type="text/css" rel="stylesheet"/>
 
     <script src="js/carousel.js"></script>
 </head>
 <body>
-
-
 
     <?php require '../src/view/homepage/navbar.php'; ?>
     <?php require '../src/view/homepage/carousel.php'; ?>
