52f00ae9d32e2cd8b3f9417d47224699b90050c4
Thevindu <kulasinghet@gmail.com>
2022-11-14 16:27:11 +0530

    Added footer to the homepage

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <html lang="en">
 <head>
+    <title>MedEx</title>
     <link href="css/footer.css"  type="text/css"/>
 </head>
+<body>
+
+
+
+    <?php require '../src/view/homepage/navbar.php'; ?>
+    <?php require  '../src/view/homepage/footer.php'; ?>
+</body>
 </html>
-
-<?php
-
-require '../src/view/homepage/navbar.php';
-require  '../src/view/homepage/footer.php';
-
-?>
-
-
--- src/view/homepage/footer.php
+++ src/view/homepage/footer.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <link href="footer.css"  type="text/css"/>
+    <link href="/css/footer.css"  type="text/css"/>
 </head>
 <footer>
     © 2022 28 Group, All rights reserved
