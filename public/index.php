ca63b1805d41ceab13ab74d8bc7f24e94fe87564
Thevindu <kulasinghet@gmail.com>
2022-11-14 14:33:28 +0530

    Added footer to the homepage

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'css/footer.css';
 require '../src/view/homepage/navbar.php';
 require  '../src/view/homepage/footer.php';
 
--- src/view/homepage/footer.php
+++ src/view/homepage/footer.php
@@ -1,5 +1,4 @@
 <html>
-<header><link rel="stylesheet" type="text/css" href="footer.css"></header>
 <footer>
     © 2022 28 Group, All rights reserved
 </footer>
