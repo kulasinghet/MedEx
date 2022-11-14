b62fb3cb09dd7f3c532b8cab4c07c88b8c9f9f0c
Thevindu <kulasinghet@gmail.com>
2022-11-14 14:31:39 +0530

    Added footer to the homepage

--- src/view/homepage/footer.php
+++ src/view/homepage/footer.php
@@ -1,5 +1,5 @@
 <html>
-<header><link rel="stylesheet" type="text/css" href="../../../public/css/footer.css"></header>
+<header><link rel="stylesheet" type="text/css" href="footer.css"></header>
 <footer>
     © 2022 28 Group, All rights reserved
 </footer>
