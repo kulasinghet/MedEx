450f0c24d8e3a9c371933a777df2090af646e546
Thevindu <kulasinghet@gmail.com>
2022-11-14 12:48:40 +0530

    Init nav bar

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
-<html lang="en">
-<head>
-    <title>MedEx</title>
-    <style>
-        p{
-            font-size: 20px;
-            /*font-weight: bold;*/
-        }
-    </style>
-</head>
-<body>
-    <h1>MedEx</h1>
-    <p>MedEx is a medical practice management system.</p>
-</body>
-</html>
+<?php
+
+include 'src/view/homepage/navbar.php';
+
+?>
---
+++ src/view/homepage/navbar.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <div class="navBar">
+            <div class="navBar__logo">
+                <a href="index.php"><img src="img/logo.png" alt="logo"></a>
+            </div>
+            <div class="navBar__menu">
+                <ul>
+                    <li><a href="/public/index.php">Home</a></li>
+                    <li><a href="/public/index.php?page=about">About</a></li>
+                    <li><a href="/public/index.php?page=contact">Contact</a></li>
+                    <li><a href="/public/index.php?page=login">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
