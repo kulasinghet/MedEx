7345b9c3270d2a56b3493086e19691e11f5e32ba
Naveen Dharmathunga <dasheenaveen@outlook.com>
2022-11-16 03:08:08 +0530

    added automatic slide changing ability

--- public/components/carousel/carousel.js
+++ public/components/carousel/carousel.js
@@ -4,6 +4,7 @@ window.onload = () => {
   const slides = carousel.querySelectorAll(".carousel-image");
   const slidesCount = slides.length;
   const maxleft = (slidesCount - 1) * 100 * -1;
+  const delay = 5000;
   let slideIndex = 0;
   let galleryPosition = 0;
 
@@ -17,14 +18,23 @@ window.onload = () => {
   carousel.querySelectorAll(".carousel-next").forEach((btn) => {
     btn.addEventListener("click", () => {
       carouselSlideChange();
+      restart();
     });
   });
 
   carousel.querySelectorAll(".carousel-prev").forEach((btn) => {
     btn.addEventListener("click", () => {
       carouselSlideChange(false);
+      restart();
     });
   });
+
+  // slides autochange function
+  let autochange = setInterval(carouselSlideChange, delay);
+  const restart = () => {
+    clearInterval(autochange);
+    autochange = setInterval(carouselSlideChange, delay);
+  };
 
   function fixImageSizes() {
     slides.forEach((img) => {
@@ -33,25 +43,24 @@ window.onload = () => {
       console.log("New img size: " + window.getComputedStyle(img).width);
     });
   }
-
+  
   function carouselSlideChange(next = true) {
     // set current slide faded
     slides[slideIndex].classList.add("fade");
-
+  
     if (next) {
-      galleryPosition +=
-        galleryPosition > maxleft ? -100 : galleryPosition * -1;
+      galleryPosition += galleryPosition > maxleft ? -100 : galleryPosition * -1;
     } else {
       galleryPosition += galleryPosition < 0 ? 100 : maxleft;
     }
-
+  
     // applying the new position for the carousel gallery
     carouselGallery.style.left = galleryPosition + "%";
     // changing the slide
     slideIndex = (galleryPosition * -1) / 100;
     // remove the fadeness from the new slide
     slides[slideIndex].classList.remove("fade");
-
+  
     // for debugging purposes
     console.log("sildes changed:" + slideIndex + "/" + slidesCount);
     console.log("New left: " + window.getComputedStyle(carouselGallery).left);
