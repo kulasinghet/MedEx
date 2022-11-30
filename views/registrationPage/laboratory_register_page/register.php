3938836d348bd654926839bad61e31a0bd735343
Madhawa Samarasinghe <madhawacha1234@gmail.com>
2022-12-01 02:47:08 +0530

    changed register pages

--- views/registrationPage/laboratory_register_page/register.php
+++ views/registrationPage/laboratory_register_page/register.php
@@ -18,9 +18,26 @@
 <input type="text" name="password" id="password" />
 <label for="retypepassword">Re-type password</label>
 <input type="text" name="retypepassword" id="retypepassword" />
+<label for="address">Address</label>
+<input type="text" name="address" id="address" />
+<label for="contactnumber">Contact Number</label>
+<input type="text" name="contactnumber" id="contactnumber" />
+<label for="business_reg_id">Business Registration Id</label>
+<input type="text" name="business_reg_id" id="business_reg_id" />
+<label for="business_reg_name">Business Registered Name</label>
+<input type="text" name="business_reg_name" id="business_reg_name" />
+<label for="laboratory_ceti_id">Laboratory Certificate Id</label>
+<input type="text" name="laboratory_ceti_id" id="laboratory_ceti_id" />
 
 
-<input type="submit" value="Next >" id="loginButton" />
+<label for="uploadbusinesscerti">Upload Business Registration Certificate</label>
+<input type="file" name="uploadbusinesscerti" id="uploadbusinesscerti" accept="image/png,image/jpeg" />
+<label for="uploadlaboratoryceti">Upload Laboratory Certificate</label>
+<input type="file" name="uploadlaboratoryceti" id="uploadlaboratoryceti" accept="image/png,image/jpeg" />
+
+
+
+<input type="submit" value="Register" id="loginButton" />
 </form>
 </body>
 </html>
--- views/registrationPage/pharmacy_register_page/phr_register.php
+++ views/registrationPage/pharmacy_register_page/phr_register.php
@@ -18,9 +18,34 @@
 <input type="text" name="password" id="password" />
 <label for="retypepassword">Re-type password</label>
 <input type="text" name="retypepassword" id="retypepassword" />
+<label for="ownername">Owner Name</label>
+<input type="text" name="ownername" id="ownername" />
+<label for="address">Address</label>
+<input type="text" name="address" id="address" />
+<label for="city">City</label>
+<input type="text" name="city" id="city" />
+<label for="contactnumber">Contact Number</label>
+<input type="text" name="contactnumber" id="contactnumber" />
+<label for="pharmacy_register_no">Pharmacy Registration No.</label>
+<input type="text" name="pharmacy_register_no" id="pharmacy_register_no" />
+<label for="business_reg_id">Business Registration Id</label>
+<input type="text" name="business_reg_id" id="business_reg_id" />
+<label for="business_reg_name">Business Registered Name</label>
+<input type="text" name="business_reg_name" id="business_reg_name" />
+<label for="pharmacy_ceti_id">Pharmacy Certificate Id</label>
+<input type="text" name="pharmacy_ceti_id" id="pharmacy_ceti_id" />
+<label for="pharmacy_ceti_name">Pharmacy Certificate Name</label>
+<input type="text" name="pharmacy_ceti_name" id="pharmacy_ceti_name" />
 
 
-<input type="submit" value="Next >" id="loginButton" />
-</form>
+
+<label for="uploadbusinesscerti">Upload Business Registration Certificate</label>
+<input type="file" name="uploadbusinesscerti" id="uploadbusinesscerti" accept="image/png,image/jpeg" />
+<label for="uploadpharceti">Upload Pharmacy Certificate</label>
+<input type="file" name="uploadpharceti" id="uploadpharceti" accept="image/png,image/jpeg" />
+
+
+
+<input type="submit" value="Register" id="loginButton" /></form>
 </body>
 </html>
