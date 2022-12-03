68ef4ed817d5e412fb1a487021a868002c3b9505
Pawandi-W <2020cs215@stu.ucsc.cmb.ac.lk>
2022-12-03 11:09:23 +0530

    SupplierLogin

---
+++ views/loginPage/supplier/auth.php
@@ -0,0 +1,24 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "medex";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$supplier_username= $_POST["username"];
+$supplier_password = $_POST["pswd"];
+$sql = "SELECT id FROM supplier  WHERE username='$supplier_username' && supplier.password='$supplier_password';";
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+  header("location: ../../dashboard/supplier/supplierDashboard.php");
+}else {
+  echo '<script type="text/javascript">alert("Wrong UserName or Password");window.location=\'login.php\';</script>';
+}
+
+
+?>
