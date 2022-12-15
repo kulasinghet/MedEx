00b68fad7f4274d44496660bb8650489ddb85d35
Madhawa Samarasinghe <madhawacha1234@gmail.com>
2022-12-16 00:54:50 +0530

    not complete viewdelivery

--- views/delivery/dashboard.php
+++ views/delivery/dashboard.php
@@ -13,41 +13,8 @@
         <p>ongoing Deliveries</p>
     </div>
     <div class="table">
+
         <table>
-            <tr>
-                <th>Order ID</th>
-                <th>Location</th>
-                <th>PharmacyId</th>
-                <th>OrderId</th>
-                <th>Status</th>
-            </tr>
-            <td>001</td>
-            <td>Kandy</td>
-            <td>W001</td>
-            <td>123</td>
-            <td>ongoing</td>
-
-
-                </tr>
-            </tr>
-            <td>001</td>
-            <td>Kandy</td>
-            <td>W001</td>
-            <td>123</td>
-            <td>ongoing</td>
-
-
-            </tr>
-            </tr>
-            <td>001</td>
-            <td>Kandy</td>
-            <td>W001</td>
-            <td>123</td>
-            <td>ongoing</td>
-
-
-            </tr>
-
 
         </table>
     </div>
