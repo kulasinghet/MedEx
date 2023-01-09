bd6854a7de40b19c38aba517854f984027916607
Pawandi-W <2020cs215@stu.ucsc.cmb.ac.lk>
2023-01-09 09:26:52 +0530

    9th Jan Commits

--- views/loginPage/supplier/login.php
+++ views/loginPage/supplier/login.php
@@ -61,43 +61,6 @@ sorigin="anonymous"
     </p>
   </div>
 </div>
-<!--footer-->
-<div style="background-color: #a6cabd;padding-top: 30%; width: auto;">
-    <footer class="footer-distributed" style="height: 100%;">
-
-        <div class="footer-left">
-    
-            <h3>Med<span>Ex</span></h3>
-    
-            <p class="footer-links">
-                <a href="#" class="link-1">Home</a>
-                <a href="#">About</a>
-    
-                <a href="#">Contact</a>
-            </p>
-    
-    
-        </div>
-
-        <div class="footer-center">
-    
-    
-            <p class="footer-site-tm">
-                <span>Team Members</span>
-            <li class="tm-color">R.D.T.D. Kulasinghe </li>
-            <li class="tm-color">I.A.P.P. Wijegunawardana </li>
-            <li class="tm-color">W.D.D.N. Dharmathunga </li>
-            <li class="tm-color">M.C.W. Samarasinghe </li>
-            </p>
-        </div>
-    
-        <div class="footer-right">
-            <p class="footer-site-right">© 2022 Group 28, All Right reserved</p>
-    
-        </div>
-    
-    </footer>
-    </div>
 
 
   </body>
