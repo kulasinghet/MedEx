20d3f14068ab5331b8137f9a952c3806a1fc6236
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-01-11 11:16:30 +0530

    Optimized grid system, moved abstract methods to abstract folder
    Grid rows and also columns are now dynamic, even without JavaScript. Now it is rendering very fast

--- public/js/g28-style.js
+++ public/js/g28-style.js
@@ -1,30 +1,3 @@
-function initGrid() {
-  document.querySelectorAll(".grid").forEach(grid => {
-    let max_row = 1;
-
-    Array.from(grid.children).forEach(child => {
-      child.classList.forEach(cls => {
-        // considering grid rows
-        if (cls.startsWith('g-row-')) {
-          // https://www.regular-expressions.info/lookaround.html
-          let row = Number(cls.match(/(?<=g-row-)[0-9]+/i));
-          let span = Number(cls.match(/(?<!g-row-)[0-9]+/i));
-          // console.log("row: " + row + " span: " + span); // for debugging
-
-          // calculating the max value of the row that used in the grid
-          let value = row + ((span !== 0)? span - 1 : 0);
-          if (max_row < value) {
-            max_row = value;
-          }
-        }
-      })
-    });
-
-    // setting template grid rows of the grid
-    grid.style.gridTemplateRows = `repeat(${max_row}, 1fr)`;
-  });
-}
-
 function initDropDown() {
   document.querySelectorAll(".dropdown").forEach(dropdown => {
     const btn = dropdown.querySelector(".btn");
@@ -60,6 +33,5 @@ function initDropDown() {
 
 window.onload = () => {
   // Initializing components
-  initGrid();
   // initDropDown(); // not included with the new scss framework
 }
