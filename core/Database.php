868fe1ed0d13b20ee7b4e6ad32dba2b9fec2c889
Thevindu Kulasinghe <kulasinghet@gmail.com>
2023-04-08 15:23:47 +0530

    add env support

--- core/Database.php
+++ core/Database.php
@@ -3,26 +3,36 @@
 namespace app\core;
 
 use mysqli;
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 
 class Database
 {
 
-    private $host = "medex-do-user-10529241-0.b.db.ondigitalocean.com";
-    private $port = "25060";
-    private $username = "doadmin";
-    private $password = "AVNS_1XxLJaaCdF9zuxtb5bR";
-    private $database = "medex";
-    private $sslmode = "REQUIRED";
     private mysqli $db;
 
     public function getConnection()
     {
-        $this->db = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
+//        $this->db = new mysqli($this->parse_env('DB_HOST'), $this->username, $this->password, $this->database, $this->port);
+        $this->db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE'], $_ENV['DB_PORT']);
+
         if ($this->db->connect_error) {
             Logger::logError("Connection failed: " . $this->db->connect_error);
             die("Connection failed: " . $this->db->connect_error);
         }
+
+
         return $this->db;
+    }
+
+    private function parse_env(string $string)
+    {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        return $_ENV[$string];
     }
 
 
