012ad3f546e93bfd1990d4e2b27f3d95a4e6dd2e
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-04-09 15:59:41 +0530

    Added custom element for SelectBox.

--- public/js/g28-forms.js
+++ public/js/g28-forms.js
@@ -1,3 +1,13 @@
+let stage = 'development';
+
+
+// Utils
+const logger = (str) => {
+  if (stage === 'development') {
+    console.log(str);
+  }
+}
+
 // Initializing Selectors
 document.querySelectorAll('.selector-group').forEach(selectorGrp => {
   const label = selectorGrp.querySelector('label');
@@ -7,5 +17,45 @@ document.querySelectorAll('.selector-group').forEach(selectorGrp => {
   }
 });
 
+// Initializing SelectBoxes
+const selectBoxes = {};
+
+document.querySelectorAll('details.form-input').forEach((selectBox, index) => {
+  const list = selectBox.querySelector('ul');
+
+  selectBoxes[index] = {
+    timeout: null
+  };
+
+  list.addEventListener("mouseenter", (e) => {
+    if (e.target === list) {
+      clearTimeout(selectBoxes[index].timeout);
+      logger("mouseenter triggered on the list of SelectBox[" + index + "]");
+    }
+  });
+
+  list.addEventListener("mouseleave", (e) => {
+    if (e.target === list) {
+      selectBoxes[index].timeout = setTimeout(() => {
+        selectBox.removeAttribute('open');
+        logger("mouseleave triggered on the list of SelectBox[" + index + "]");
+      }, 1500);
+    }
+  });
+
+  list.addEventListener("click", () => {
+    setTimeout(() => {
+      selectBox.removeAttribute('open');
+      logger("mouseclick triggered on the list of SelectBox[" + index + "]");
+    }, 100);
+
+    // prevent triggering mouseleave event
+    setTimeout(() => {
+      clearTimeout(selectBoxes[index].timeout);
+    }, 1000);
+  });
+})
+
+
 // confirmation
-console.log('Forms: JS loaded successfully!');
+logger('Forms: JS loaded successfully!');
