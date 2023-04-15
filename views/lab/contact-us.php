c98bf3fbdaa358a4391a26b14fffffaec32c6d19
Pawandi-W <2020cs215@stu.ucsc.cmb.ac.lk>
2023-04-15 19:18:37 +0530

    New styles Applied for Lab Views

--- views/lab/contact-us.php
+++ views/lab/contact-us.php
@@ -4,25 +4,53 @@
     <meta charset="UTF-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <title>Contact Us</title>
-    <link href="../scss/vendor/demo.css" rel="stylesheet" />
-    <link href="../css/supplier/formcss.css" rel="stylesheet" />
     <!-- Font awesome kit -->
     <script crossorigin="anonymous" src="https://kit.fontawesome.com/9b33f63a16.js"></script>
+    <!-- g28 style -->
+    <link rel="stylesheet" href="../scss/vendor/demo.css" />
+    <script src="../js/g28-main.js"></script>
 </head>
 
 <body>
-
-    <nav>
-        <div class="nav-search">
-            <form onsubmit="preventDefault();" role="search">
-                <label for="search">Search for stuff</label>
-                <input autofocus id="search" placeholder="Search..." required type="search" />
-                <button type="submit">Go</button>
-            </form>
+    <!-- Section: Fixed Components -->
+    <div class="sidebar-collapsible">
+        <div class="sidebar-inner">
+            <nav class="sidebar-header">
+                <div class="sidebar-logo">
+                    <a href="#">
+                        <img alt="MedEx logo" src="../res/logo/logo-text_light.svg" />
+                    </a>
+                </div>
+            </nav>
+            <div class="sidebar-context">
+                <ul class="main-buttons">
+                    <li>
+                        <a href="#"> <i class="fa fa-file-text-o"></i>Lab Requests</a>
+                        <ul class="hidden">
+                            <li><a href="/lab/requests">Accept Lab Requests</a></li>
+                            <li><a href="#">View Accepted Requests</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"> <i class="fa fa-list-alt"></i> Lab Reports</a>
+                        <ul class="hidden">
+                            <li><a href="/lab/reports"> Generate Lab Reports</a></li>
+                            <li><a href="#">View Past Lab Reports </a></li>
+                        </ul>
+                    </li>
+                    <li class="disabled">
+                        <a href="lab/contact-us"> <i class="fa fa-phone"></i> Contact Us </a>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </div>
+    <nav>
         <div class="nav-inner">
             <ul>
-                <li><a href="/login"><i class="fa fa-sign-out"></i></a></li>
+                <li><a class="link" href="#"><i class="fa-solid fa-gear"></i></a></li>
+                <li><a class="link" href="login"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+                <li><a class="link" href="#"><i class="fa-solid fa-bell"></i></a></li>
             </ul>
             <a class="nav-profile" href="#">
                 <div class="nav-profile-image">
@@ -31,60 +59,37 @@
             </a>
         </div>
     </nav>
-
-    <div class="sidebar">
-        <div class="sidebar-inner">
-            <nav class="sidebar-header">
-                <div class="sidebar-logo">
-                    <a href="/dashboard">
-                        <img alt="MedEx logo" src="../res/logo/logo-text_light.svg" />
-                    </a>
-                </div>
-            </nav>
-            <div class="sidebar-context">
-                <h6 class="sidebar-context-title">Menu</h6>
-                <ul>
-                    <li>
-                        <a class="btn" href="/lab/requests"> <i class="fa fa-check"></i> Accept Lab Requests
-                        </a>
-                    </li>
-                    <li>
-                        <a class="btn" href="/lab/reports"> <i class="fa fa-file-text-o"></i> Provide Lab Reports </a>
-                    </li>
-
-                    <li>
-                        <a class="btn disabled" href="/lab/contact-us"> <i class="fa fa-phone"></i> Contact Us </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
+    <!-- Section: Fixed Components -->
 
     <div class="canvas nav-cutoff sidebar-cutoff">
         <div class="canvas-inner">
             <div class="row">
                 <div class="col">
-                    <div class="card g-col-2 g-row-2-start-3"
-                        style=" box-shadow: 0 3px 10px rgb(0 0 0 / 0.2); border-radius: 20px; width:70%; padding-bottom:2%;">
-                        <div class="card-body">
 
-                            <div style="padding: 2%;">
-                                <h3> Contact Us </h3>
-                                <form>
-                                    <input type='text' name='subject' class='input-box' placeholder='Enter Subject'
-                                        required><br><br>
-                                    <textarea id="contactus" name="message" rows="20" cols="50">Enter Message
-                                   </textarea>
-                                    <br>
-                                    <input type='submit' value='Submit' class='btn btn--primary'>
-                                </form>
+                    <div class="contact-us">
+                        <h1>Contact Us</h1>
+
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Subject</label>
+                                <input type="email" class="form-input" id="exampleFormControlInput1"
+                                    placeholder="Subject">
                             </div>
-                        </div>
+
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Message</label>
+                                <textarea class="form-input" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Send Message</button>
+                        </form>
+
                     </div>
+
+
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
--- views/supplier/register.php
+++ views/supplier/register.php
@@ -15,8 +15,8 @@
     <script src="https://kit.fontawesome.com/9b33f63a16.js" crossorigin="anonymous"></script>
 </head>
 
-<body style="background-color: #e4e8e5; width:100%;">
-    <div style="padding:5%;">
+<body style="background-color: #e4e8e5; width:100%; height: 100%;">
+    <div style="padding:1%;">
         <div class="card g-col-2 g-row-2-start-3"
             style=" box-shadow: 0 3px 10px rgb(0 0 0 / 0.2); border-radius: 20px;">
             <div class="card-body" style="display:flex; flex-direction: row;">
@@ -38,11 +38,12 @@
                             <input type="text" name="email" class="form-input" placeholder="Enter Email" required><br>
                             <input type="text" name="address" class="form-input" placeholder="Enter Address"
                                 required><br>
+                            <input type="text" name="mobile" class="form-input" placeholder="Enter Mobile Phone Number"
+                                required><br>
 
                         </div>
                         <div style="width: 50%; padding:2%">
-                            <input type="text" name="mobile" class="form-input" placeholder="Enter Mobile Phone Number"
-                                required><br>
+
                             <input type="text" name="supRegNum" class="form-input"
                                 placeholder="Enter Supplier Registration Number" required><br>
                             <input type="text" name="busiRegNum" class="form-input"
@@ -50,13 +51,17 @@
                             <input type="text" name="supCertId" class="form-input"
                                 placeholder="Enter Supplier Certificate ID" required><br>
                             Business Registration Certificate <small>(3Mb - jpg,jpeg,png,pdf) </small><br><input
-                                type="file" name="BusRegiCert" id="BusRegiCert" required accept="image/*,.pdf"><br><br>
+                                class="form-input" type="file" name="BusRegiCert" id="BusRegiCert" required
+                                accept="image/*,.pdf"><br>
                             Supplier Registration Certificate <small>(3Mb - jpg,jpeg,png,pdf) </small><br><input
-                                type="file" name="SuppRegiCert" id="SuppRegiCert" required
-                                accept="image/*,.pdf"><br><br>
+                                class="form-input" type="file" name="SuppRegiCert" id="SuppRegiCert" required
+                                accept="image/*,.pdf"><br>
                             <input type="submit" value="Create Account" class="btn btn--primary">
                         </div>
-                        <form>
+                    </form>
+                    <h6 style="padding:1%; text-align:center">Already have an account? <a href="/login">Log
+                            in</a></h6>
+
                 </div>
 
             </div>
