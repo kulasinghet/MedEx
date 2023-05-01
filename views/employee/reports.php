5a73e9e43dd2f9a5345f6bfaa8fe43edbb8a5cf2
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-05-02 00:40:29 +0530

    Added report item template

--- views/employee/reports.php
+++ views/employee/reports.php
@@ -39,81 +39,29 @@ echo $components->createNavbar();
 <!-- Section: Dashboard Layout -->
 <div class="canvas nav-cutoff sidebar-cutoff">
     <div class="canvas-inner">
-        <!-- Toolbox -->
-        <div class="toolbox">
-            <div class="block row">
-                <div class="col">
-                    <div class="nav-search">
-                        <form onsubmit="preventDefault();" role="search">
-                            <label for="filter-by-search">Search for stuff</label>
-                            <input autofocus id="filter-by-search" placeholder="Search..." required type="search"/>
-                            <button type="submit">Go</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="separator"></div>
-            <form class="block row" method="POST" action="">
-                <div class="col">
-                    <label for="sort-by">Sort by: </label>
-                </div>
-                <div class="col">
-                    <g28-selectbox id="sort-by" placeholder="Default">
-                        Default, Name, Reg Date
-                    </g28-selectbox>
-                </div>
-            </form>
-        </div>
-        <!-- Toolbox -->
-
         <!-- Content -->
         <div class="row margin-bottom">
-            <div class="col">
-                <table class="table approval-table">
-                    <thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                        <th class="c" style="max-width: 100px">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    try {
-                        $controller = new EmployeeResListController();
-                        $approvals = match ($filter) {
-                            'pharmacy' => $controller->getPharmacyList(no_of_approvals, $set),
-                            'supplier' => $controller->getSupplierList(no_of_approvals, $set),
-                            'lab' => $controller->getLabList(no_of_approvals, $set),
-                            'delivery' => $controller->getDeliveryList(no_of_approvals, $set),
-                            default => throw new Exception("Invalid filter!"),
-                        };
-                        if (!empty($approvals)) {
-                            for ($i = 0; $i < no_of_approvals; $i++) {
-                                if (array_key_exists($i, $approvals)) {
-                                    echo $components->createResItem($approvals[$i]);
-                                } else {
-                                    echo "<tr class='empty'>";
-                                    echo "<td colspan='5'></td>";
-                                    echo "</tr>";
-                                }
-                            }
-                        } else {
-                            echo "<tr class='empty'>";
-                            echo "<td class='no-data' colspan='5' rowspan='".no_of_approvals."'>No records found!</td>";
-                            echo "</tr>";
-                            for ($i = 0; $i < no_of_approvals - 1; $i++) {
-                                echo "<tr></tr>";
-                            }
-                        }
-                    } catch (Exception $e) {
-                        echo "Something went wrong!";
-                    }
-                    ?>
-                    </tbody>
-                </table>
+            <div class="col card report-list">
+                <div class="card-body list-content">
+                    <div class="report-itm">
+                        <div class="report-inner">
+                            <div class="header">
+                                <div class="header-icon">
+                                    <a>
+                                        <i class="fa-solid fa-suitcase-medical"></i>
+                                    </a>
+                                </div>
+                                <div class="header-data">
+                                    <h6 class="header-username">Username</h6>
+                                    <h5 class="header-subject">Subject</h5>
+                                </div>
+                            </div>
+                            <div class="report-body">
+                                <p>Message</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- Content -->
