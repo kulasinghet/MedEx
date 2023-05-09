ab27b621253c1a808e9f5610ca49aae7037e5aee
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-05-09 11:18:47 +0530

    fixed notifications

--- models/HyperEntities/HyperPharmacyModel.php
+++ models/HyperEntities/HyperPharmacyModel.php
@@ -75,15 +75,15 @@ class HyperPharmacyModel extends HyperEntityModel
             $stmt->execute();
 
             if ($stmt->affected_rows == 1) {
-                $store->setNotification('Pharmacy is verified!', $this->username . ' is verified successfully.', 'success');
+                $store->setNotification('Pharmacy is '.($action? 'verified' : 'ignored').'!', $this->username . ' is '.($action? 'verified' : 'ignored').' successfully.', 'success');
                 return true;
             } else {
-                $store->setNotification('Pharmacy verification error!', $this->username . ' couldn\'t be verified (see logs).', 'error');
+                $store->setNotification('Pharmacy verification error!', $this->username . ' couldn\'t do the operation (see logs).', 'error');
                 Logger::logError($stmt->error);
                 return false;
             }
         } catch (\Exception $e) {
-            $store->setNotification('Pharmacy verification error!', $this->username . ' couldn\'t be verified (see logs).', 'error');
+            $store->setNotification('Pharmacy verification error!', $this->username . ' couldn\'t do the operation (see logs).', 'error');
             Logger::logError($e->getMessage());
             return false;
         }
